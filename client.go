d4dddc785e483b21a8ad28fa0ea49a9ea4a582dc
ManviArora <manvi_arora@cargill.com>
2020-08-20 01:01:36 +0530

    Added 404 status code for retry

--- client.go
+++ client.go
@@ -437,7 +437,7 @@ func DefaultRetryPolicy(ctx context.Context, resp *http.Response, err error) (bo
 	// the server time to recover, as 500's are typically not permanent
 	// errors and may relate to outages on the server side. This will catch
 	// invalid response codes as well, like 0 and 999.
-	if resp.StatusCode == 0 || (resp.StatusCode >= 500 && resp.StatusCode != 501) {
+	if resp.StatusCode == 0 || resp.StatusCode == 404 || (resp.StatusCode >= 500 && resp.StatusCode != 501) {
 		return true, nil
 	}
 
